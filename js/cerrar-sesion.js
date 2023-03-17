fdfd2d2e3b051dc7ef31ab4c8921e7a382bbc19b
Aivan <dominguezmiguel715@gmail.com>
2023-03-17 00:12:45 -0700

    Funcionalidad temporalmente movida a menu-lateral.js.

--- js/cerrar-sesion.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('[name="cerrarSesion"]').addEventListener('click', () => {
-    fetch('php/includes/cerrarsesion.inc.php')
-        .then((respuesta) => respuesta.json())
-        .then((datos) => (location.href = datos.contenido));
-});

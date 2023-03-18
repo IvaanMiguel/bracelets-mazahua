c61ffdb197be4409572b7219200c20aab8b10d1f
Aivan <dominguezmiguel715@gmail.com>
2023-03-18 01:30:11 -0700

    Indentación cambiada de 4 espacios a 2 espacios.

--- index.php
+++ index.php
@@ -4,73 +4,73 @@
 <html lang='es'>
 
 <head>
-    <meta charset='UTF-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <meta charset='UTF-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
 
-    <!-- Roboto Google Fonts -->
-    <link rel='preconnect' href='https://fonts.googleapis.com'>
-    <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
-    <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap' rel='stylesheet'>
+  <!-- Roboto Google Fonts -->
+  <link rel='preconnect' href='https://fonts.googleapis.com'>
+  <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+  <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap' rel='stylesheet'>
 
-    <!-- Material Design Google Icons -->
-    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' />
+  <!-- Material Design Google Icons -->
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' />
 
-    <link rel='stylesheet' href='css/global.css'>
-    <link rel='stylesheet' href='css/index.css'>
+  <link rel='stylesheet' href='css/global.css'>
+  <link rel='stylesheet' href='css/index.css'>
 
-    <title>Bracelets Mazahua</title>
+  <title>Bracelets Mazahua</title>
 
-    <script src='components/md-icono/md-icono.js' type='module' defer></script>
-    <script src='components/md-boton/md-boton.js' type='module' defer></script>
-    <script src='components/menu-lateral/menu-lateral.js' type='module' defer></script>
+  <script src='components/md-icono/md-icono.js' type='module' defer></script>
+  <script src='components/md-boton/md-boton.js' type='module' defer></script>
+  <script src='components/menu-lateral/menu-lateral.js' type='module' defer></script>
 </head>
 
 <body>
-    <main class='contenedor'>
-        <menu-lateral class='menu-lateral primario' data-clase-reducido='menu-lateral--reducido'>
-            <button class='boton boton--icono primario primario-2-texto boton--active-primario' is='md-boton' data-evento='alternarmenu'>
-                <md-icono class='icono-chico' data-icono='menu'></md-icono>
-            </button>
-            <div class='menu-lateral__secciones'>
-                <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cargarseccion' name='inicio'>
-                    <md-icono class='icono-chico' data-icono='home'></md-icono>
-                    <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Inicio</span>
-                </button>
-                <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cargarseccion' name='nuevoPedido'>
-                    <md-icono class='icono-chico' data-icono='shopping_basket'></md-icono>
-                    <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Nuevo pedido</span>
-                </button>
-                <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cargarseccion' name='clientes'>
-                    <md-icono class='icono-chico' data-icono='groups'></md-icono>
-                    <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Clientes</span>
-                </button>
-                <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cargarseccion' name='productos'>
-                    <md-icono class='icono-chico' data-icono='inventory_2'></md-icono>
-                    <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Productos</span>
-                </button>
-                <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cargarseccion' name='pedidos'>
-                    <md-icono class='icono-chico' data-icono='inventory'></md-icono>
-                    <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Pedidos</span>
-                </button>
-            </div>
-            <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cerrarsesion'>
-                <md-icono class='icono-chico' data-icono='logout'></md-icono>
-                <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Cerrar sesión</span>
-            </button>
-        </menu-lateral>
-        <div class='contenido'>
-            <div class='cabecera fondo-2-texto'>
-                <h1 class='cabecera__titulo titulo-grande'>Bracelets Mazahua</h1>
-                <span class='cabecera__nombre titulo-mediano'>
-                    <?= isset($_SESSION['nombreUsuario']) ? $_SESSION['nombreUsuario'] : '' ?>
-                </span>
-            </div>
-            <div class='seccion' data-rol='secciones'>
-                <?php include_once 'php/pages/inicio.php' ?>
-            </div>
-        </div>
-    </main>
+  <main class='contenedor'>
+    <menu-lateral class='menu-lateral primario' data-clase-reducido='menu-lateral--reducido'>
+      <button class='boton boton--icono primario primario-2-texto boton--active-primario' is='md-boton' data-evento='alternarmenu'>
+        <md-icono class='icono-chico' data-icono='menu'></md-icono>
+      </button>
+      <div class='menu-lateral__secciones'>
+        <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cargarseccion' name='inicio'>
+          <md-icono class='icono-chico' data-icono='home'></md-icono>
+          <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Inicio</span>
+        </button>
+        <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cargarseccion' name='nuevoPedido'>
+          <md-icono class='icono-chico' data-icono='shopping_basket'></md-icono>
+          <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Nuevo pedido</span>
+        </button>
+        <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cargarseccion' name='clientes'>
+          <md-icono class='icono-chico' data-icono='groups'></md-icono>
+          <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Clientes</span>
+        </button>
+        <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cargarseccion' name='productos'>
+          <md-icono class='icono-chico' data-icono='inventory_2'></md-icono>
+          <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Productos</span>
+        </button>
+        <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cargarseccion' name='pedidos'>
+          <md-icono class='icono-chico' data-icono='inventory'></md-icono>
+          <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Pedidos</span>
+        </button>
+      </div>
+      <button class='boton boton--icono-texto primario primario-2-texto boton--active-primario' data-clase-reducido='boton--icono' is='md-boton' data-evento='cerrarsesion'>
+        <md-icono class='icono-chico' data-icono='logout'></md-icono>
+        <span class='etiqueta etiqueta-grande' data-clase-reducido='etiqueta--oculto' data-rol='etiqueta'>Cerrar sesión</span>
+      </button>
+    </menu-lateral>
+    <div class='contenido'>
+      <div class='cabecera fondo-2-texto'>
+        <h1 class='cabecera__titulo titulo-grande'>Bracelets Mazahua</h1>
+        <span class='cabecera__nombre titulo-mediano'>
+          <?= isset($_SESSION['nombreUsuario']) ? $_SESSION['nombreUsuario'] : '' ?>
+        </span>
+      </div>
+      <div class='seccion' data-rol='secciones'>
+        <?php include_once 'php/pages/inicio.php' ?>
+      </div>
+    </div>
+  </main>
 </body>
 
 </html>
--- inicio-sesion.php
+++ inicio-sesion.php
@@ -1,69 +1,72 @@
 <!DOCTYPE html>
 <html lang='es'>
+
 <head>
-    <meta charset='UTF-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <meta charset='UTF-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
 
-    <!-- Roboto Google Fonts -->
-    <link rel='preconnect' href='https://fonts.googleapis.com'>
-    <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
-    <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap' rel='stylesheet'>
+  <!-- Roboto Google Fonts -->
+  <link rel='preconnect' href='https://fonts.googleapis.com'>
+  <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+  <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap' rel='stylesheet'>
 
-    <!-- Material Design Google Icons -->
-    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' />
+  <!-- Material Design Google Icons -->
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' />
 
-    <link rel='stylesheet' href='css/global.css'>
-    <link rel='stylesheet' href='css/estilos.css'>
-    <link rel='stylesheet' href='css/formularios-usuario.css'>
+  <link rel='stylesheet' href='css/global.css'>
+  <link rel='stylesheet' href='css/estilos.css'>
+  <link rel='stylesheet' href='css/formularios-usuario.css'>
 
-    <script src='components/md-boton/md-boton.js' type='module' defer></script>
-    <script src='components/md-enlace/md-enlace.js' type='module' defer></script>
-    <script src='components/campo-texto/campo-texto.js' type='module' defer></script>
+  <script src='components/md-boton/md-boton.js' type='module' defer></script>
+  <script src='components/md-enlace/md-enlace.js' type='module' defer></script>
+  <script src='components/campo-texto/campo-texto.js' type='module' defer></script>
 
-    <title>Bracelets Mazahua</title>
+  <title>Bracelets Mazahua</title>
 </head>
+
 <body>
-    <main class='contenedor'>
-        <div class='contenido'>
-            <img class='contenido__logo' src='images/logo.png' alt='Bracelets Mazahua'>
-            <form class='formulario'>
-                <h2 class='titulo-grande'>Iniciar sesión</h2>
-                <div class='campos'>
-                    <campo-texto data-clase-etiqueta='etiqueta'>
-                        <span class='cuerpo-mediano' slot='etiqueta-texto'>Nombre de usuario o dirección de email</span>
-                        <input
-                            class='fondo fondo-2-texto cuerpo-mediano'
-                            slot='campo'
-                            type='text'
-                            value='<?= ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['nombreUsuario'])) ? $_GET['nombreUsuario'] : '' ?>'
-                            name='idUsuario'>
-                    </campo-texto>
-                    <label class='input'>
-                        <span class='cuerpo-mediano'>Contraseña</span>
-                        <div class='input-clave'>
-                            <input class='input__campo campo--clave cuerpo-mediano fondo fondo-2-texto' type='password' name='clave'>
-                            <span class='material-symbols-outlined icono icono-visibilidad icono-mediano icono--absolute' title='Mostrar contraseña'>visibility</span>
-                        </div>
-                    </label>
-                </div>
-                <button class='boton boton-inicio-sesion primario primario-2-texto boton--active-primario' is='md-boton'>
-                    <span class='etiqueta-grande'>Iniciar sesión</span>
-                </button>
-            </form>
-        </div>
-        <div class='divisor'>
-            <div class='divisor__texto'>
-                <div class='linea'></div>
-                <span class='texto cuerpo-chico'>¿No tienes una cuenta?</span>
-                <div class='linea'></div>
+  <main class='contenedor'>
+    <div class='contenido'>
+      <img class='contenido__logo' src='images/logo.png' alt='Bracelets Mazahua'>
+      <form class='formulario'>
+        <h2 class='titulo-grande'>Iniciar sesión</h2>
+        <div class='campos'>
+          <campo-texto data-clase-etiqueta='etiqueta'>
+            <span class='cuerpo-mediano' slot='etiqueta-texto'>Nombre de usuario o dirección de email</span>
+            <input
+              class='fondo fondo-2-texto cuerpo-mediano'
+              slot='campo'
+              type='text'
+              value='<?= ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['nombreUsuario'])) ? $_GET['nombreUsuario'] : '' ?>'
+              name='idUsuario'>
+          </campo-texto>
+          <label class='input'>
+            <span class='cuerpo-mediano'>Contraseña</span>
+            <div class='input-clave'>
+              <input class='input__campo campo--clave cuerpo-mediano fondo fondo-2-texto' type='password' name='clave'>
+              <span class='material-symbols-outlined icono icono-visibilidad icono-mediano icono--absolute' title='Mostrar contraseña'>visibility</span>
             </div>
-            <a class='boton primario primario-2-texto boton--active-primario' is='md-enlace' href='registro.php'>
-                <span class='etiqueta-grande'>Crear una cuenta</span>
-            </a>
+          </label>
         </div>
-    </main>
-    <script src='js/classes/notificacion.class.js'></script>
-    <script src='js/inicio-sesion.js'></script>
+        <button class='boton boton-inicio-sesion primario primario-2-texto boton--active-primario' is='md-boton'>
+          <span class='etiqueta-grande'>Iniciar sesión</span>
+        </button>
+      </form>
+    </div>
+    <div class='divisor'>
+      <div class='divisor__texto'>
+        <div class='linea'></div>
+        <span class='texto cuerpo-chico'>¿No tienes una cuenta?</span>
+        <div class='linea'></div>
+      </div>
+      <a class='boton primario primario-2-texto boton--active-primario' is='md-enlace' href='registro.php'>
+        <span class='etiqueta-grande'>Crear una cuenta</span>
+      </a>
+    </div>
+  </main>
+  <script src='js/classes/notificacion.class.js'></script>
+  <script src='js/inicio-sesion.js'></script>
 </body>
+
 </html>

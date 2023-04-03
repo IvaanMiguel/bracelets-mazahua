4c161883535ff0b02f3b4f18fe1838d4d3580858
Aivan <dominguezmiguel715@gmail.com>
2023-04-02 21:16:03 -0700

    Implementación de utils.js

--- js/inicio-sesion.js
+++ js/inicio-sesion.js
@@ -1,4 +1,4 @@
-import { NotificacionError } from '../components/notificacion-error/notificacion-error.js';
+import { utils } from './utils.js';
 
 document.querySelector('.boton-inicio-sesion').addEventListener('click', (e) => {
   e.preventDefault();
@@ -11,34 +11,6 @@ document.querySelector('.boton-inicio-sesion').addEventListener('click', (e) => 
     .then((datos) => {
       document.querySelectorAll('.notificacion-error').forEach((e) => e.remove());
 
-      switch (datos.tipo) {
-        case 'url':
-          location.href = datos.contenido;
-          break;
-
-        case 'mensaje':
-          const notificacionError = new NotificacionError();
-          const span = document.createElement('span');
-
-          span.setAttribute('slot', 'error');
-          span.innerText = datos.contenido.mensaje;
-          notificacionError.appendChild(span);
-
-          document.querySelector('.campos').after(notificacionError);
-          break;
-
-        case 'array':
-          datos.contenido.forEach((item) => {
-            const notificacionError = new NotificacionError();
-            const span = document.createElement('span');
-
-            span.setAttribute('slot', 'error');
-            span.innerText = item.mensaje;
-            notificacionError.appendChild(span);
-
-            document.querySelector('.campos').after(notificacionError);
-          });
-          break;
-      }
+      utils.obtenerRespuesta(datos);
     });
 });
--- js/registro.js
+++ js/registro.js
@@ -1,4 +1,4 @@
-import { NotificacionError } from '../components/notificacion-error/notificacion-error.js';
+import { utils } from './utils.js';
 
 document.querySelector('.boton-registro').addEventListener('click', (e) => {
   e.preventDefault();
@@ -11,42 +11,6 @@ document.querySelector('.boton-registro').addEventListener('click', (e) => {
     .then((datos) => {
       document.querySelectorAll('.notificacion-error').forEach((e) => e.remove());
 
-      switch (datos.tipo) {
-        case 'url':
-          location.href = datos.contenido;
-          break;
-
-        case 'mensaje':
-          const notificacionError = new NotificacionError();
-          const span = document.createElement('span');
-
-          span.setAttribute('slot', 'error');
-          span.innerText = item.mensaje;
-          notificacionError.appendChild(span);
-
-          document.querySelector('.campos').after(notificacionError);
-          break;
-
-        case 'array':
-          datos.contenido.forEach((item) => {
-            const notificacionError = new NotificacionError();
-            const span = document.createElement('span');
-
-            span.setAttribute('slot', 'error');
-            span.innerText = item.mensaje;
-            notificacionError.appendChild(span);
-
-            switch (item.ambito) {
-              case 'general':
-                document.querySelector('.campos').after(notificacionError);
-                break;
-
-              default:
-                document.querySelector(`[name='${item.ambito}']`).parentElement.after(notificacionError);
-                break;
-            }
-          });
-          break;
-      }
+      utils.obtenerRespuesta(datos);
     });
 });

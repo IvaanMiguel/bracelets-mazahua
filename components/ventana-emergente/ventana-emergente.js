283adbc78183362eda427405b338a0bf5b8a3f06
Aivan <dominguezmiguel715@gmail.com>
2023-04-04 01:31:33 -0700

    Web component ventana-emergente creado.

---
+++ components/ventana-emergente/ventana-emergente.js
@@ -0,0 +1,50 @@
+import { componentsUtil } from '../components-util.js';
+
+export class VentanaEmergente extends HTMLElement {
+  constructor () {
+    super();
+
+    this.divContenedor = document.createElement('div');
+
+    this.estilosPadre = document.createElement('link');
+
+    this.estilosClases = document.createElement('link');
+
+    this.attachShadow({ mode: 'open' });
+
+    componentsUtil.establecerAtributos(this.estilosPadre, {
+      rel: 'stylesheet',
+      href: 'components/ventana-emergente/ventana-emergente.css'
+    });
+
+    componentsUtil.establecerAtributos(this.estilosClases, {
+      rel: 'stylesheet',
+      href: 'css/clases.css'
+    });
+
+    this.shadowRoot.appendChild(this.estilosClases);
+    this.shadowRoot.appendChild(this.estilosPadre);
+    this.shadowRoot.appendChild(this.shadowRoot.host.firstElementChild);
+  }
+
+  connectedCallback () {
+    this.shadowRoot.addEventListener('cerrarventana', () => this.cerrarVentana());
+    this.shadowRoot.addEventListener('cerrarsesion', () => this.cerrarSesion());
+  }
+
+  cerrarVentana () {
+    const host = this.shadowRoot.host;
+
+    if (host.getAttribute('style').indexOf('display: flex;') > -1) {
+      host.removeAttribute('style');
+    }
+  }
+
+  cerrarSesion () {
+    fetch('php/includes/cerrarsesion.inc.php')
+      .then((respuesta) => respuesta.json())
+      .then((datos) => (location.href = datos.contenido));
+  }
+}
+
+customElements.define('ventana-emergente', VentanaEmergente);

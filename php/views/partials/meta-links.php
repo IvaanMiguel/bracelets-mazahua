4fe30cc577cb30aaac48e52e08e77699be7ab23c
Aivan <dominguezmiguel715@gmail.com>
2023-04-05 22:04:50 -0700

    Creación de archivo parcial contenedor principalmente de etiquetas meta y links utilizados en todas las páginas.

---
+++ php/views/partials/meta-links.php
@@ -0,0 +1,15 @@
+<meta charset='UTF-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+
+<!-- Roboto Google Fonts -->
+<link rel='preconnect' href='https://fonts.googleapis.com'>
+<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+<link href='https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap' rel='stylesheet'>
+
+<!-- Material Design Google Icons -->
+<link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' />
+
+<link rel='stylesheet' href='css/global.css'>
+
+<title>Bracelets Mazahua</title>

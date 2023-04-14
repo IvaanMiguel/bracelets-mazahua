cd2c083f5abebeaf66669b1ce167849b612670f6
Aivan <dominguezmiguel715@gmail.com>
2023-04-14 16:40:07 -0700

    Componente divisor creado.

---
+++ components/wc-divisor/wc-divisor.js
@@ -0,0 +1,53 @@
+const template = document.createElement('template');
+template.innerHTML = /*html*/`
+<style>
+  :host {
+    --clr-divisor: var(--clr-primario-40);
+
+    display: block;
+  }
+
+  :host([data-orientacion='horizontal']) {
+    width: 100%;
+    max-height: 0px;
+    border-bottom: 1px solid var(--clr-divisor);
+  }
+
+  :host([data-orientacion='vertical']) {
+    height: 100%;
+    max-width:0px;
+    border-left: 1px solid var(--clr-divisor);
+  }
+</style>
+`;
+
+class WCDivisor extends HTMLElement {
+  static observedAttributes = ['data-orientacion'];
+
+  constructor () {
+    super();
+
+    this.dataOrientacion = 'horizontal';
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  get dataOrientacion () { return this.dataset.orientacion; }
+
+  set dataOrientacion (orientacion) {
+    if (this.dataOrientacion === orientacion) return;
+    this.dataset.orientacion = orientacion;
+  }
+
+  attributeChangedCallback (name, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    switch (name) {
+      case 'data-orientacion':
+        this.dataOrientacion = newValue;
+        break;
+    }
+  }
+}
+
+customElements.define('wc-divisor', WCDivisor);

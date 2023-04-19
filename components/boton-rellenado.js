7bd7924237565a995e6dcdab96093719a249bdff
Aivan <dominguezmiguel715@gmail.com>
2023-04-18 18:56:47 -0700

    Primera versión de boton-rellenado creada.

---
+++ components/boton-rellenado.js
@@ -0,0 +1,39 @@
+import WCBoton from './wc-boton.js';
+
+const CSS = new CSSStyleSheet();
+CSS.replaceSync(/*css*/`
+button:hover::after, a:hover::after {
+  background-color: var(--clr-hover);
+}
+
+button:hover, a:hover {
+  box-shadow: var(--bs-elevacion-1);
+}
+
+button:active, a:active {
+  box-shadow: unset;
+}
+
+button:active::after, a:active::after {
+  background-color: var(--clr-active);
+}
+
+button:focus-visible, a:focus-visible {
+  outline: none;
+  box-shadow: unset;
+}
+
+button:focus-visible::after, a:focus-visible::after {
+  background-color: var(--clr-active);
+}
+`);
+
+class BotonRellenado extends WCBoton {
+  constructor () {
+    super();
+
+    this.shadowRoot.adoptedStyleSheets = [...this.shadowRoot.adoptedStyleSheets, CSS];
+  }
+}
+
+customElements.define('boton-rellenado', BotonRellenado);

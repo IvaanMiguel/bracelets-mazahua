b8ff575221c454123b522d87747052b97e1662c3
Aivan <dominguezmiguel715@gmail.com>
2023-04-18 18:31:03 -0700

    Constructable stylesheet implementada dentro del constructor de la clase y propiedades data-color-fondo y data-color-texto agregadas.

--- components/wc-boton.js
+++ components/wc-boton.js
@@ -1,67 +1,79 @@
-const template = document.createElement('template');
-template.innerHTML = /*html*/`
-<style>
-  :host {
-    --clr-after: transparent;
-
-    display: flex;
-  }
-
-  button, a {
-    display: inline-flex;
-    align-items: center;
-
-    text-align: left;
-    text-decoration: none;
-    font-family: inherit;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    
-    overflow: hidden;
-    height: 2.5rem;
-    box-sizing: border-box;
-    padding: 0 1.5rem;
-    cursor: pointer;
-    position: relative;
-
-    border: none;
-    border-radius: 1.25rem;
-
-    transition: 
-      box-shadow 0.2s ease 0s,
-      background-color 0.2s ease 0s,
-      color 0.2s ease 0s,
-      padding 0.2s ease 0s,
-      gap 0.2s ease 0s,
-      max-width 0.2s ease 0s;
-  }
-
-  button::after, a::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    background-color: var(--clr-after);
-  }
-
-  ::slotted([slot='etiqueta']) {
-    font-size: var(--fs-etiqueta-grande);
-    line-height: var(--lh-etiqueta-grande);
-    letter-spacing: var(--ls-etiqueta-grande);
-    font-weight: var(--fw-medio);
-  }
-</style>
-`;
-
-class WCBoton extends HTMLElement {
-  static observedAttributes = ['href', 'type'];
+export default class WCBoton extends HTMLElement {
+  static observedAttributes = ['href', 'type', 'data-color-fondo', 'data-color-texto'];
 
   constructor () {
+    if (new.target === WCBoton) {
+      throw new Error('No puede ser instanciada una clase abstracta.');
+    }
+
     super();
 
+    this._CSS = new CSSStyleSheet();
+    this._CSS.replaceSync(/*css*/`
+    :host {
+      --clr-hover: transparent;
+      --clr-active: transparent;
+      --clr-fondo: transparent;
+      --clr-texto: #000000;
+    
+      display: inline-flex;
+    }
+    
+    button, a {
+      display: inline-flex;
+      align-items: center;
+    
+      text-align: left;
+      text-decoration: none;
+      font-family: inherit;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      color: var(--clr-texto);
+      
+      overflow: hidden;
+      height: 2.5rem;
+      box-sizing: border-box;
+      padding: 0 1.5rem;
+      cursor: pointer;
+      position: relative;
+    
+      border: none;
+      border-radius: 1.25rem;
+      background-color: var(--clr-fondo);
+    
+      transition: 
+        box-shadow 0.2s ease 0s,
+        background-color 0.2s ease 0s,
+        color 0.2s ease 0s,
+        padding 0.2s ease 0s,
+        gap 0.2s ease 0s,
+        max-width 0.2s ease 0s;
+    }
+    
+    button::after, a::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      background-color: transparent;
+    
+      transition: background-color .2s ease 0s;
+    }
+    
+    ::slotted([slot='etiqueta']) {
+      font-size: var(--fs-etiqueta-grande);
+      line-height: var(--lh-etiqueta-grande);
+      letter-spacing: var(--ls-etiqueta-grande);
+      font-weight: var(--fw-medio);
+    }
+    `);
+
     this._onClick = this._onClick.bind(this);
+    this._reglaCSSHost = Array.from(this._CSS.cssRules).find(reglaCSS => {
+      return reglaCSS.selectorText === ':host';
+    });
 
     this._etiquetaSlot = document.createElement('slot');
     this._etiquetaSlot.setAttribute('name', 'etiqueta');
@@ -69,8 +81,7 @@ class WCBoton extends HTMLElement {
     this._iconoSlot = document.createElement('slot');
     this._iconoSlot.setAttribute('name', 'icono');
 
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.attachShadow({ mode: 'open' }).adoptedStyleSheets = [this._CSS];
 
     this._determinarBotonSchrodinger();
     this.shadowRoot.appendChild(this._botonSchrodinger);
@@ -121,6 +132,14 @@ class WCBoton extends HTMLElement {
 
   set type (tipoBoton) { this.setAttribute('type', tipoBoton); }
 
+  get dataColorFondo () { return this.dataset.colorFondo; }
+
+  set dataColorFondo (color) { this.dataset.color = color; }
+
+  get dataColorTexto () { return this.dataset.colorTexto; }
+
+  set dataColorTexto (color) { this.dataset.colorTexto = color; }
+
   connectedCallback () {
     this._botonSchrodinger.addEventListener('click', this._onClick);
   }
@@ -142,6 +161,20 @@ class WCBoton extends HTMLElement {
 
         const esBoton = oldValue !== null && newValue === null;
         this._reconstruirBotonSchrodinger(esBoton);
+        break;
+
+      case 'data-color-fondo':
+        this._reglaCSSHost.style.setProperty('--clr-fondo', !newValue ? 'transparent' : newValue);
+        break;
+
+      case 'data-color-texto':
+        this._reglaCSSHost.style.setProperty('--clr-texto', !newValue ? '#000000' : newValue);
+
+        const colorHover = getComputedStyle(this).getPropertyValue('--clr-texto') + '14'; // .08
+        this._reglaCSSHost.style.setProperty('--clr-hover', !newValue ? 'transparent' : colorHover);
+
+        const colorActive = getComputedStyle(this).getPropertyValue('--clr-texto') + '1f'; // .12
+        this._reglaCSSHost.style.setProperty('--clr-active', !newValue ? 'transparent' : colorActive);
         break;
     }
   }

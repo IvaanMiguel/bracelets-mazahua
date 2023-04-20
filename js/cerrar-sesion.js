6d23892cb9abb96833e336ac1d85a96332918c56
Aivan <dominguezmiguel715@gmail.com>
2023-04-19 22:15:15 -0700

    Export removido y event listeners confirmarcierresesion y cerrarsesion agregados.

--- js/cerrar-sesion.js
+++ js/cerrar-sesion.js
@@ -1,5 +1,9 @@
-export function cerrarSesion () {
+document.addEventListener('confirmarcierresesion', () => {
+  document.querySelector('[data-id="cerrar-sesion"]').setAttribute('style', 'display: flex;');
+});
+
+document.addEventListener('cerrarsesion', () => {
   fetch('php/includes/cerrar_sesion.inc.php')
     .then((respuesta) => respuesta.json())
     .then((datos) => (location.href = datos.contenido));
-}
+});

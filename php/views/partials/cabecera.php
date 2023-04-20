99c9e68ff02650b238fed258c47e6f10efcd5832
Aivan <dominguezmiguel715@gmail.com>
2023-04-19 21:58:44 -0700

    Archivo con cabecera añadido.

---
+++ php/views/partials/cabecera.php
@@ -0,0 +1,6 @@
+<div class='cabecera'>
+  <h1 class='cabecera__titulo titulo-grande'>Bracelets Mazahua</h1>
+  <span class='cabecera__nombre titulo-mediano'>
+    <?= isset($_SESSION['nombreUsuario']) ? $_SESSION['nombreUsuario'] : ''; ?>
+  </span>
+</div>

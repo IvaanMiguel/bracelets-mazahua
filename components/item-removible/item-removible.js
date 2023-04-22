cf1a68ba91162ac4cc0ff789786d7a98624fe180
Aivan <dominguezmiguel715@gmail.com>
2023-04-21 19:45:47 -0700

    Constructed stylesheet implementado y adición de template cambiado al constructor.

--- components/item-removible/item-removible.js
+++ components/item-removible/item-removible.js
@@ -1,6 +1,36 @@
+const hojaCSS = new CSSStyleSheet();
+hojaCSS.replaceSync(/*css*/`
+  :host {
+    display: flex;
+    flex-direction: column;
+
+    box-sizing: border-box;
+  }
+
+  div {
+    display: flex;
+    gap: var(--espaciado-chico);
+    align-items: center;
+  
+    flex: 1;
+  
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  
+    padding: var(--espaciado-chico) var(--espaciado-jumbo);
+  }
+
+  ::slotted([slot='info']) {
+    flex: 1;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+`);
+
 const template = document.createElement('template');
 template.innerHTML = /*html*/`
-  <link rel='stylesheet' href='components/item-removible/item-removible.css'>
   <div>
     <slot name='info'></slot>
     <slot name='boton-remover'></slot>
@@ -13,12 +43,13 @@ class ItemRemovible extends HTMLElement {
 
     this._connectedCallbackCalled = false;
 
-    this.attachShadow({ mode: 'open' });
+    this.attachShadow({ mode: 'open' }).adoptedStyleSheets = [hojaCSS];
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   confirmarRemoverUbicacion (e) {
     document.querySelector('[data-id="remover-ubicacion"]').style.display = 'flex';
-    document.querySelector('[data-id="remover-ubicacion"]').detallesEvento = e.detail.ubicacion;
+    document.querySelector('[data-id="remover-ubicacion"]').detallesEvento = e.detail;
   }
 
   connectedCallback () {
@@ -26,8 +57,6 @@ class ItemRemovible extends HTMLElement {
     this._connectedCallbackCalled = true;
 
     document.body.addEventListener('confirmarremoverubicacion', this.confirmarRemoverUbicacion);
-
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 }
 

acbeef588e24721687a830ef6474daadddc07507
Aivan <dominguezmiguel715@gmail.com>
2023-05-01 23:52:36 -0700

    Lógica para eliminar una categoría existente implementada.

---
+++ js/categories/eliminar-categoria.js
@@ -0,0 +1,37 @@
+(() => {
+  const ventanaEliminar = document.getElementById('eliminar-categoria');
+
+  document.addEventListener('eliminarcategoria', (e) => {
+    const listaItem = e.target.parentElement;
+
+    ventanaEliminar.querySelectorAll('.nombre-categoria').forEach((elemento) => (elemento.innerText = listaItem.innerText));
+    ventanaEliminar.mostrarVentana();
+    ventanaEliminar.idCategoria = listaItem.parentElement.querySelector('.id-categoria').value;
+    ventanaEliminar.listaItem = listaItem;
+  });
+
+  ventanaEliminar.addEventListener('cancelar', () => ventanaEliminar.cerrarVentana());
+
+  ventanaEliminar.addEventListener('confirmareliminarcategoria', () => {
+    const formData = new FormData();
+    formData.append('idCategoria', ventanaEliminar.idCategoria);
+
+    fetch('php/includes/categories/eliminar_categoria.inc.php', {
+      method: 'POST',
+      body: formData
+    })
+      .then((respuesta) => respuesta.json())
+      .then((datos) => {
+        if (datos.status !== 1) return;
+
+        ventanaEliminar.cerrarVentana();
+        ventanaEliminar.listaItem.remove();
+
+        const categorias = document.querySelectorAll('lista-controlador item-divisor lista-item');
+        console.log(categorias);
+        document.getElementById('categorias-titulo').innerText = categorias.length === 1
+          ? '1 categoría'
+          : `${categorias.length} categorías`;
+      });
+  });
+})();

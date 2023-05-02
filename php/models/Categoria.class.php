83154a027bbdc0e3b8d6c77499145ad597aeff65
Aivan <dominguezmiguel715@gmail.com>
2023-05-02 00:03:13 -0700

    Métodos actualizar categoría y eliminar categoría implementados.

--- php/models/Categoria.class.php
+++ php/models/Categoria.class.php
@@ -8,15 +8,30 @@ class Categoria extends Dbh
 {
   protected function obtenerCategorias(): array|false
   {
-    $stmt = $this->conectar()->prepare('SELECT * FROM categoriaproducto ORDER BY LOWER(nombreCategoria)  ASC;');
+    $stmt = $this->conectar()->prepare('SELECT * FROM categoriaproducto ORDER BY LOWER(nombreCategoria) ASC;');
     $this->ejecutarSentencia($stmt);
 
     return $stmt->fetchAll();
   }
 
-  protected function crearCategoria(string $nombreCategoria): void
+  protected function actualizarCategoria(int $idCategoria, string $nombreCategoria)
   {
-    $stmt = $this->conectar()->prepare('INSERT INTO categoriaproducto VALUES(0, ?);');
+    $stmt = $this->conectar()->prepare('UPDATE categoriaproducto SET nombreCategoria = ? WHERE idCategoriaProducto = ?');
+    $this->ejecutarSentencia($stmt, array($nombreCategoria, $idCategoria));
+  }
+
+  protected function crearCategoria(string $nombreCategoria): string|false
+  {
+    $conexion = $this->conectar();
+    $stmt = $conexion->prepare('INSERT INTO categoriaproducto VALUES(0, ?);');
     $this->ejecutarSentencia($stmt, array($nombreCategoria));
+
+    return $conexion->lastInsertId();
+  }
+
+  protected function eliminarCategoria(int $idCategoria)
+  {
+    $stmt = $this->conectar()->prepare('DELETE FROM categoriaproducto WHERE idCategoriaProducto = ?;');
+    $this->ejecutarSentencia($stmt, array($idCategoria));
   }
 }

13114743fa1cfc1249f303bed497c6d16f533c34
Aivan <dominguezmiguel715@gmail.com>
2023-05-02 00:07:43 -0700

    Método modificar y remover categoria implementados y atributo idCategoria añadido.

--- php/controllers/Categoria.class.php
+++ php/controllers/Categoria.class.php
@@ -7,10 +7,12 @@ use \classes\Respuesta;
 class Categoria extends \models\Categoria
 {
   private array $errores = [];
-  private string $nombreCategoria;
+  private ?int $idCategoria;
+  private ?string $nombreCategoria;
 
-  public function __construct(string $nombreCategoria)
+  public function __construct(?string $nombreCategoria = null, ?int $idCategoria = null)
   {
+    $this->idCategoria = $idCategoria;
     $this->nombreCategoria = $nombreCategoria;
   }
 
@@ -18,20 +20,55 @@ class Categoria extends \models\Categoria
   {
     if ($this->camposVacios()) {
       array_push($this->errores, Respuesta::CAMPO_VACIO);
+    } else {
+      $this->validarCategoria();
     }
 
+
     if (count($this->errores) > 0) {
-      $respuesta = new Respuesta(Respuesta::ARRAY, $this->errores);
+      $respuesta = new Respuesta(Respuesta::STATUS_ERROR, Respuesta::ARRAY, $this->errores);
       exit($respuesta->Json());
     }
 
-    $this->crearCategoria($this->nombreCategoria);
+    $categoriaId = $this->crearCategoria($this->nombreCategoria);
 
-    echo (new Respuesta(Respuesta::MENSAJE, 'Éxito padrino.'))->Json();
+    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, array($this->nombreCategoria, $categoriaId)))->Json();
+  }
+
+  public function modificarCategoria(): void
+  {
+    if ($this->camposVacios()) {
+      array_push($this->errores, Respuesta::CAMPO_VACIO);
+    } else {
+      $this->validarCategoria();
+    }
+
+    if (count($this->errores) > 0) {
+      $respuesta = new Respuesta(Respuesta::STATUS_ERROR, Respuesta::ARRAY, $this->errores);
+      exit($respuesta->Json());
+    }
+
+    $this->actualizarCategoria($this->idCategoria, $this->nombreCategoria);
+
+    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::MENSAJE, 'Éxito'))->Json();
+  }
+
+  public function removerCategoria()
+  {
+    $this->eliminarCategoria($this->idCategoria);
+
+    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::MENSAJE, 'Éxito'))->Json();
   }
 
   private function camposVacios(): bool
   {
     return (empty($this->nombreCategoria));
+  }
+
+  private function validarCategoria(): void
+  {
+    if (!ctype_alpha($this->nombreCategoria)) {
+      array_push($this->errores, Respuesta::CATEGORIA_INVALIDA);
+    }
   }
 }

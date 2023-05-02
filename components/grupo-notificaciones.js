7c03b5359e523b5305726f81a06eb69fa8a557dd
Aivan <dominguezmiguel715@gmail.com>
2023-05-01 23:48:38 -0700

    Componente grupo-notificaciones creado.

---
+++ components/grupo-notificaciones.js
@@ -0,0 +1,49 @@
+const hojaCSS = /*css*/`
+  :host {
+    display: flex;
+    flex-direction: column;
+    gap: var(--espaciado-grande);
+
+    position: absolute;
+    top: 0;
+    right: 0;
+
+    box-sizing: border-box;
+    overflow: hidden auto;
+    
+    width: max-content;
+    max-height: 100%;
+    padding: var(--espaciado-jumbo);
+  }
+`;
+
+const template = document.createElement('template');
+template.innerHTML = /*html*/`
+  <slot></slot>
+`;
+
+class GrupoNotificaciones extends HTMLElement {
+  constructor () {
+    super();
+
+    this._CSS = new CSSStyleSheet();
+    this._CSS.replaceSync(hojaCSS);
+
+    this._childListObserver = new MutationObserver(() => {
+      if (this.childElementCount === 0) this.remove();
+    });
+
+    this.attachShadow({ mode: 'open' }).adoptedStyleSheets = [this._CSS];
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback () {
+    this._childListObserver.observe(this, { childList: true });
+  }
+
+  disconnectedCallback () {
+    this._childListObserver.disconnect();
+  }
+}
+
+customElements.define('grupo-notificaciones', GrupoNotificaciones);

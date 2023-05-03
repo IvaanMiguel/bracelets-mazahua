279e8d6d06ff036e8d2424e6c7f4db577f1694f3
Aivan <dominguezmiguel715@gmail.com>
2023-05-02 19:22:12 -0700

    Constantes para longitud del nombre añadidas, sanitización de espacios en blanco agregada, validación para caracteres especiales en el nombre agregada y métodos para buscar una categoría ya existente y validar la longitud del nombre de la categoría implementados.

--- php/controllers/Categoria.class.php
+++ php/controllers/Categoria.class.php
@@ -6,6 +6,9 @@ use \classes\Respuesta;
 
 class Categoria extends \models\Categoria
 {
+  public const NOMBRE_MIN_LONGITUD = 4;
+  public const NOMBRE_MAX_LONGITUD = 30;
+
   private array $errores = [];
   private ?int $idCategoria;
   private ?string $nombreCategoria;
@@ -18,10 +21,15 @@ class Categoria extends \models\Categoria
 
   public function registrarCategoria(): void
   {
+    $this->nombreCategoria = trim($this->nombreCategoria);
+    $this->nombreCategoria = preg_replace('/\s+/', ' ', $this->nombreCategoria);
+
     if ($this->camposVacios()) {
       array_push($this->errores, Respuesta::CAMPO_VACIO);
     } else {
-      $this->validarCategoria();
+      $this->validarNombreCategoria();
+      $this->buscarCategoriaExistente();
+      $this->validarLongitud();
     }
 
 
@@ -30,9 +38,9 @@ class Categoria extends \models\Categoria
       exit($respuesta->Json());
     }
 
-    $categoriaId = $this->crearCategoria($this->nombreCategoria);
+    $idCategoria = $this->crearCategoria($this->nombreCategoria);
 
-    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, array($this->nombreCategoria, $categoriaId)))->Json();
+    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, array($this->nombreCategoria, $idCategoria, Respuesta::CATEGORIA_REGISTRADA)))->Json();
   }
 
   public function modificarCategoria(): void
@@ -40,7 +48,9 @@ class Categoria extends \models\Categoria
     if ($this->camposVacios()) {
       array_push($this->errores, Respuesta::CAMPO_VACIO);
     } else {
-      $this->validarCategoria();
+      $this->validarNombreCategoria();
+      $this->buscarCategoriaExistente();
+      $this->validarLongitud();
     }
 
     if (count($this->errores) > 0) {
@@ -50,14 +60,14 @@ class Categoria extends \models\Categoria
 
     $this->actualizarCategoria($this->idCategoria, $this->nombreCategoria);
 
-    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::MENSAJE, 'Éxito'))->Json();
+    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, array(Respuesta::CATEGORIA_MODIFICADA)))->Json();
   }
 
   public function removerCategoria()
   {
     $this->eliminarCategoria($this->idCategoria);
 
-    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::MENSAJE, 'Éxito'))->Json();
+    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, array(Respuesta::CATEGORIA_ELIMINADA)))->Json();
   }
 
   private function camposVacios(): bool
@@ -65,10 +75,30 @@ class Categoria extends \models\Categoria
     return (empty($this->nombreCategoria));
   }
 
-  private function validarCategoria(): void
+  private function validarNombreCategoria(): void
   {
-    if (!ctype_alpha($this->nombreCategoria)) {
+    $caracteresEspeciales = array('á', 'é', 'í', 'ó', 'ú', 'ñ', 'ü', 'Á', 'É', 'Í', 'Ó', 'Ú', 'Ñ', 'Ü', ' ');
+    $caracteresNormalizados = array('a', 'e', 'i', 'o', 'u', 'n', 'u', 'A', 'E', 'I', 'O', 'U', 'N', 'U', '');
+    $nombreCategoria = str_replace($caracteresEspeciales, $caracteresNormalizados, $this->nombreCategoria);
+
+    if (!ctype_alpha($nombreCategoria)) {
       array_push($this->errores, Respuesta::CATEGORIA_INVALIDA);
+    }
+  }
+
+  private function buscarCategoriaExistente(): void
+  {
+    if ($this->categoriaExistente($this->nombreCategoria)) {
+      array_push($this->errores, Respuesta::CATEGORIA_EXISTENTE);
+    }
+  }
+
+  private function validarLongitud(): void
+  {
+    if (strlen($this->nombreCategoria) < self::NOMBRE_MIN_LONGITUD) {
+      array_push($this->errores, Respuesta::CATEGORIA_NOMBRE_CORTO);
+    } else if (strlen($this->nombreCategoria) > self::NOMBRE_MAX_LONGITUD) {
+      array_push($this->errores, Respuesta::CATEGORIA_NOMBRE_LARGO);
     }
   }
 }

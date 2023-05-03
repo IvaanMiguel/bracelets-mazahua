c5340865322513874278e81bf53c526c4274ff10
Aivan <dominguezmiguel715@gmail.com>
2023-05-02 19:18:20 -0700

    Soporte para recibir notificaciones por parte del servidor añadido.

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,8 @@
 import ItemError from '../components/item-error.js';
+import GrupoNotificaciones from '../components/grupo-notificaciones.js';
+import NotificacionFlotante from '../components/notificacion-flotante.js';
 
-export default (function () {
+export default (() => {
   const obtenerRespuesta = (datos, generalCallback) => {
     document.querySelectorAll('[slot="error"]').forEach((itemError) => itemError.remove());
 
@@ -9,18 +11,40 @@ export default (function () {
         location.href = datos.contenido;
         break;
 
-      case 1: // Array de errores por parte del servidor.
+      case 1: // Array de respuestas por parte del servidor.
         datos.contenido.forEach((item) => {
-          const itemError = new ItemError();
-          itemError.setAttribute('slot', 'error');
-          itemError.innerHTML = /*html*/`
-            <md-icono slot='icono' data-icono='error' data-opsz='20' data-escala='s'></md-icono>
-            <wc-texto slot='error' data-tipo-fuente='cuerpo-s'>${item.mensaje}</wc-texto>
-          `;
+          if (!item.hasOwnProperty('ambito')) return;
+
+          let itemError;
+          if (item.ambito !== 'notificacion') {
+            itemError = new ItemError();
+            itemError.setAttribute('slot', 'error');
+            itemError.innerHTML = /*html*/`
+              <md-icono slot='icono' data-icono='error' data-opsz='20' data-escala='s'></md-icono>
+              <wc-texto slot='error' data-tipo-fuente='cuerpo-s'>${item.mensaje}</wc-texto>
+            `;
+          }
 
           switch (item.ambito) {
             case 'general':
               generalCallback(itemError);
+              break;
+
+            case 'notificacion':
+              const grupoNotificaciones = document.querySelector('grupo-notificaciones')
+                ? document.querySelector('grupo-notificaciones')
+                : new GrupoNotificaciones();
+
+              const notificacionFlotante = new NotificacionFlotante();
+              notificacionFlotante.dataset.tipo = datos.status ? 'exito' : 'error';
+
+              notificacionFlotante.innerHTML = `
+                <wc-texto data-tipo-fuente='titulo-m'>${item.titulo}</wc-texto>
+                <wc-texto>${item.mensaje}</wc-texto>
+              `;
+
+              grupoNotificaciones.appendChild(notificacionFlotante);
+              document.body.appendChild(grupoNotificaciones);
               break;
 
             default:

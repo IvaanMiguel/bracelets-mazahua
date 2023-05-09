32ac9035773bd7077d010a141c43e0c86ba28fbd
Aivan <dominguezmiguel715@gmail.com>
2023-05-08 18:45:20 -0700

    Mensajes para el usuario añadidos.

--- php/controllers/Categoria.class.php
+++ php/controllers/Categoria.class.php
@@ -6,6 +6,48 @@ use \classes\Respuesta;
 
 class Categoria extends \models\Categoria
 {
+  public const CATEGORIA_INVALIDA = [
+    'titulo' => 'Nombre de categoría inválida',
+    'mensaje' => 'El nombre de la categoría solo debe contener letras y espacios.',
+    'ambito' => 'general'
+  ];
+
+  public const CATEGORIA_REGISTRADA = [
+    'titulo' => 'Nueva categoría añadida',
+    'mensaje' => 'La categoría ha sido añadida con éxito.',
+    'ambito' => 'notificacion'
+  ];
+
+  public const CATEGORIA_MODIFICADA = [
+    'titulo' => 'Categoría modificada',
+    'mensaje' => 'La categoría ha sido modificada con éxito.',
+    'ambito' => 'notificacion'
+  ];
+
+  public const CATEGORIA_ELIMINADA = [
+    'titulo' => 'Categoría eliminada',
+    'mensaje' => 'La categoría ha sido eliminada con éxito.',
+    'ambito' => 'notificacion'
+  ];
+
+  public const CATEGORIA_EXISTENTE = [
+    'titulo' => 'Categoría ya existente',
+    'mensaje' => 'La categoría ingresada ya existe.',
+    'ambito' => 'notificacion'
+  ];
+
+  public const CATEGORIA_NOMBRE_CORTO = [
+    'titulo' => 'Nombre muy corto',
+    'mensaje' => 'El nombre de la categoría debe tener como mínimo ' . self::NOMBRE_MIN_LONGITUD . ' caracteres.',
+    'ambito' => 'general'
+  ];
+
+  public const CATEGORIA_NOMBRE_LARGO = [
+    'titulo' => 'Nombre muy largo',
+    'mensaje' => 'El nombre de la categoría debe tener como máximo ' . self::NOMBRE_MAX_LONGITUD . ' caracteres.',
+    'ambito' => 'general'
+  ];
+
   public const NOMBRE_MIN_LONGITUD = 4;
   public const NOMBRE_MAX_LONGITUD = 30;
 
@@ -32,7 +74,6 @@ class Categoria extends \models\Categoria
       $this->validarLongitud();
     }
 
-
     if (count($this->errores) > 0) {
       $respuesta = new Respuesta(Respuesta::STATUS_ERROR, Respuesta::ARRAY, $this->errores);
       exit($respuesta->Json());
@@ -40,7 +81,7 @@ class Categoria extends \models\Categoria
 
     $idCategoria = $this->crearCategoria($this->nombreCategoria);
 
-    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, array($this->nombreCategoria, $idCategoria, Respuesta::CATEGORIA_REGISTRADA)))->Json();
+    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, array($this->nombreCategoria, $idCategoria, self::CATEGORIA_REGISTRADA)))->Json();
   }
 
   public function modificarCategoria(): void
@@ -60,14 +101,14 @@ class Categoria extends \models\Categoria
 
     $this->actualizarCategoria($this->idCategoria, $this->nombreCategoria);
 
-    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, array(Respuesta::CATEGORIA_MODIFICADA)))->Json();
+    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, array(self::CATEGORIA_MODIFICADA)))->Json();
   }
 
   public function removerCategoria()
   {
     $this->eliminarCategoria($this->idCategoria);
 
-    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, array(Respuesta::CATEGORIA_ELIMINADA)))->Json();
+    echo (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, array(self::CATEGORIA_ELIMINADA)))->Json();
   }
 
   private function camposVacios(): bool
@@ -82,23 +123,23 @@ class Categoria extends \models\Categoria
     $nombreCategoria = str_replace($caracteresEspeciales, $caracteresNormalizados, $this->nombreCategoria);
 
     if (!ctype_alpha($nombreCategoria)) {
-      array_push($this->errores, Respuesta::CATEGORIA_INVALIDA);
+      array_push($this->errores, self::CATEGORIA_INVALIDA);
     }
   }
 
   private function buscarCategoriaExistente(): void
   {
     if ($this->categoriaExistente($this->nombreCategoria)) {
-      array_push($this->errores, Respuesta::CATEGORIA_EXISTENTE);
+      array_push($this->errores, self::CATEGORIA_EXISTENTE);
     }
   }
 
   private function validarLongitud(): void
   {
     if (strlen($this->nombreCategoria) < self::NOMBRE_MIN_LONGITUD) {
-      array_push($this->errores, Respuesta::CATEGORIA_NOMBRE_CORTO);
+      array_push($this->errores, self::CATEGORIA_NOMBRE_CORTO);
     } else if (strlen($this->nombreCategoria) > self::NOMBRE_MAX_LONGITUD) {
-      array_push($this->errores, Respuesta::CATEGORIA_NOMBRE_LARGO);
+      array_push($this->errores, self::CATEGORIA_NOMBRE_LARGO);
     }
   }
 }

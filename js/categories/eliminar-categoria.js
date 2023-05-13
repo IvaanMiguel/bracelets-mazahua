8542f8ec2b9b12f09acb9d2c21b0a89be789ac35
Aivan <dominguezmiguel715@gmail.com>
2023-05-13 09:49:06 -0700

    IIFE innecesaria removida e imports actualizados.

--- js/categories/eliminar-categoria.js
+++ js/categories/eliminar-categoria.js
@@ -1,42 +1,40 @@
-import utils from '../utils.js';
+import { obtenerRespuesta } from '../vista-control.js';
 
-(() => {
-  const ventanaEliminar = document.getElementById('eliminar-categoria');
+const ventanaEliminar = document.getElementById('eliminar-categoria');
 
-  document.addEventListener('eliminarcategoria', (e) => {
-    const listaItem = e.target.parentElement;
+document.addEventListener('eliminarcategoria', (e) => {
+  const listaItem = e.target.parentElement;
 
-    ventanaEliminar.querySelectorAll('.nombre-categoria').forEach((elemento) => {
-      elemento.innerText = listaItem.innerText;
+  ventanaEliminar.querySelectorAll('.nombre-categoria').forEach((elemento) => {
+    elemento.innerText = listaItem.innerText;
+  });
+  ventanaEliminar.mostrarVentana();
+  ventanaEliminar.idCategoria = listaItem.parentElement.querySelector('.id-categoria').value;
+  ventanaEliminar.listaItem = listaItem;
+});
+
+ventanaEliminar.addEventListener('cancelar', () => ventanaEliminar.cerrarVentana());
+
+ventanaEliminar.addEventListener('confirmareliminarcategoria', () => {
+  const formData = new FormData();
+  formData.append('idCategoria', ventanaEliminar.idCategoria);
+
+  fetch('php/includes/categories/eliminar_categoria.inc.php', {
+    method: 'POST',
+    body: formData
+  })
+    .then((respuesta) => respuesta.json())
+    .then((datos) => {
+      obtenerRespuesta(datos);
+
+      if (datos.status !== 1) return;
+
+      ventanaEliminar.cerrarVentana();
+      ventanaEliminar.listaItem.remove();
+
+      const categorias = document.querySelectorAll('lista-controlador item-divisor lista-item');
+      document.getElementById('categorias-titulo').innerText = categorias.length === 1
+        ? '1 categoría'
+        : `${categorias.length} categorías`;
     });
-    ventanaEliminar.mostrarVentana();
-    ventanaEliminar.idCategoria = listaItem.parentElement.querySelector('.id-categoria').value;
-    ventanaEliminar.listaItem = listaItem;
-  });
-
-  ventanaEliminar.addEventListener('cancelar', () => ventanaEliminar.cerrarVentana());
-
-  ventanaEliminar.addEventListener('confirmareliminarcategoria', () => {
-    const formData = new FormData();
-    formData.append('idCategoria', ventanaEliminar.idCategoria);
-
-    fetch('php/includes/categories/eliminar_categoria.inc.php', {
-      method: 'POST',
-      body: formData
-    })
-      .then((respuesta) => respuesta.json())
-      .then((datos) => {
-        utils.obtenerRespuesta(datos);
-
-        if (datos.status !== 1) return;
-
-        ventanaEliminar.cerrarVentana();
-        ventanaEliminar.listaItem.remove();
-
-        const categorias = document.querySelectorAll('lista-controlador item-divisor lista-item');
-        document.getElementById('categorias-titulo').innerText = categorias.length === 1
-          ? '1 categoría'
-          : `${categorias.length} categorías`;
-      });
-  });
-})();
+});

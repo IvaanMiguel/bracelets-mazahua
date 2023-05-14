80966101d4cf251130ece4b30712475bb50c08bb
Aivan <dominguezmiguel715@gmail.com>
2023-05-13 17:04:52 -0700

    Variables actualizadas.

--- php/includes/customers/agregar_cliente.inc.php
+++ php/includes/customers/agregar_cliente.inc.php
@@ -7,8 +7,7 @@ require_once AUTOLOADER;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $nombre = htmlspecialchars($_POST['nombre'], ENT_QUOTES, 'UTF-8');
-  $primerApellido = htmlspecialchars($_POST['primerApellido'], ENT_QUOTES, 'UTF-8');
-  $segundoApellido = htmlspecialchars($_POST['segundoApellido'], ENT_QUOTES, 'UTF-8');
+  $apellidos = htmlspecialchars($_POST['apellidos'], ENT_QUOTES, 'UTF-8');
   $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
   $celular = htmlspecialchars($_POST['celular'], ENT_QUOTES, 'UTF-8');
   $edad = htmlspecialchars($_POST['edad'], ENT_QUOTES, 'UTF-8');
@@ -16,11 +15,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   $cliente = Cliente::crearClienteConstructor(
       $nombre,
-      $primerApellido,
-      $segundoApellido,
-      $edad,
-      $celular,
+      $apellidos,
       $email,
+      $celular,
+      $edad,
       $ubicaciones
   );
 

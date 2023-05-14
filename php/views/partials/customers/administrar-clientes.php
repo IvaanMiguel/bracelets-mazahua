ea5b0ff9980bafd9f62b4f72880b06a729a26d2c
Aivan <dominguezmiguel715@gmail.com>
2023-05-13 17:16:46 -0700

    Lista de clientes implementada.

--- php/views/partials/customers/administrar-clientes.php
+++ php/views/partials/customers/administrar-clientes.php
@@ -1,37 +1,28 @@
+<?php
+  require_once AUTOLOADER;
+
+  use \controllers\Cliente;
+
+  $vistaCliente = Cliente::vistaClienteConstructor();
+  $clientes = $vistaCliente->mostrarClientes();
+?>
+
 <lista-encabezada>
-  <h1 class='titulo-grande' slot='titulo'>6 clientes</h1>
-  <lista-controlador slot='lista'>
-    <item-divisor>
-      <lista-item>
-        <span slot='info-principal'>Item 1</span>
-        <span slot='info-extra'>Elemento 1</span>
-      </lista-item>
-    </item-divisor>
-    <item-divisor>
-      <lista-item>
-        <span slot='info-principal'>Item 1</span>
-        <span slot='info-extra'>Elemento 1</span>
-      </lista-item>
-    </item-divisor>
-    <item-divisor>
-      <lista-item>
-        <span slot='info-principal'>Item 1</span>
-        <span slot='info-extra'>Elemento 1</span>
-      </lista-item>
-    </item-divisor>
-    <item-divisor>
-      <lista-item>
-        <span slot='info-principal'>Item 1</span>
-      </lista-item>
-    </item-divisor>
-    <item-divisor>
-      <lista-item>
-        <span slot='info-principal'>Item 1</span>
-      </lista-item>
-    </item-divisor>
-      <lista-item>
-        <span slot='info-principal'>Item 1</span>
-      </lista-item>
-    </item-divisor>
+  <wc-texto id='clientes-titulo' slot='titulo' data-tipo-fuente='titulo-l'>
+    <span id='total-clientes'><?= count($clientes)  ?></span>
+    <?= 'cliente' . (count($clientes) === 1 ? '' : 's') ?>
+  </wc-texto>
+  <lista-controlador id='lista-clientes'>
+
+    <?php $i = 0;
+    foreach ($clientes as $cliente): ?>
+      <item-divisor <?= ++$i === count($clientes) ? 'data-no-divisor' : '' ?>>
+        <lista-item>
+          <wc-texto data-tipo-fuente='titulo-s'><?= $cliente['nombre'] ?></wc-texto>
+          <input class='id-cliente' type='hidden' value=<?= $cliente['id'] ?>>
+        </lista-item>
+      </item-divisor>
+    <?php endforeach; ?>
+
   </lista-controlador>
 </lista-encabezada>

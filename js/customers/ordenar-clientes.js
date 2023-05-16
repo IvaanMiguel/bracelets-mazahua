acda297d0d32494a5e76a6035b46e752fa96b5e1
Aivan <dominguezmiguel715@gmail.com>
2023-05-16 12:18:26 -0700

    Métodos para ordenar los clientes y las ubicaciones añadidos.

---
+++ js/customers/ordenar-clientes.js
@@ -0,0 +1,22 @@
+import { ordenarItems } from '../vista-control.js';
+
+const lista = document.getElementById('lista-clientes');
+
+export const ordenarClientes = () => {
+  const listaItems = lista.querySelectorAll('item-divisor');
+
+  ordenarItems(listaItems, (item, i) => {
+    item.dataNoDivisor = (listaItems.length === i);
+    lista.appendChild(item);
+  });
+};
+
+const ubicacionesCliente = document.getElementById('ubicaciones-cliente');
+
+export const ordenarClienteUbicaciones = () => {
+  const ubicacionesItems = ubicacionesCliente.querySelectorAll('option');
+
+  ordenarItems(ubicacionesItems, (ubicacion) => ubicacionesCliente.appendChild(ubicacion));
+};
+
+ordenarClientes();

a8bafd8025900ebd9e868d77b7683ab0779b66b6
Aivan <dominguezmiguel715@gmail.com>
2023-05-17 09:01:29 -0700

    Método actualizarUbicacion y eliminarUbicacion añadido.

--- php/models/Ubicacion.class.php
+++ php/models/Ubicacion.class.php
@@ -7,31 +7,69 @@ use \classes\Dbh;
 class Ubicacion extends Dbh
 {
   protected function obtenerUbicaciones(
-      int $id
+      int $idCliente
   ): array|false
   {
     $stmt = $this->conectar()->prepare('SELECT * FROM ubicacioncliente WHERE idCliente = ?;');
-    $this->ejecutarSentencia($stmt, array($id));
+    $this->ejecutarSentencia($stmt, array($idCliente));
 
     return $stmt->fetchAll();
   }
 
-  protected function agregarUbicacion(
+  protected function crearUbicacion(
       string $callePrincipal,
       string $callesAdyacentes,
+      string $colonia,
       string $numeroExterior,
       string $numeroInterior,
       string $cp,
-      string $colonia,
       int $idCliente
   ): string|false
   {
     $conexion = $this->conectar();
     $stmt = $conexion->prepare('INSERT INTO ubicacioncliente VALUES (0, ?, ?, ?, ?, ?, ?, ?);');
     $this->ejecutarSentencia($stmt, array(
-        $callePrincipal, $callesAdyacentes, $numeroExterior, $numeroInterior, $cp, $colonia, $idCliente
+        $callePrincipal, $callesAdyacentes, $colonia, $numeroExterior, $numeroInterior, $cp, $idCliente
     ));
 
     return $conexion->lastInsertId();
+  }
+
+  protected function actualizarUbicacion(
+    string $callePrincipal,
+    string $callesAdyacentes,
+    string $colonia,
+    string $numeroExterior,
+    string $numeroInterior,
+    string $cp,
+    int $id
+  ): void
+  {
+    $stmt = $this->conectar()->prepare('UPDATE ubicacioncliente SET
+      callePrincipal = ?,
+      callesAdyacentes = ?,
+      colonia = ?,
+      numeroExterior = ?,
+      numeroInterior = ?,
+      cp = ?
+      WHERE id = ?;');
+
+    $this->ejecutarSentencia($stmt, array(
+      $callePrincipal,
+      $callesAdyacentes,
+      $colonia,
+      $numeroExterior,
+      $numeroInterior,
+      $cp,
+      $id
+    ));
+  }
+
+  protected function eliminarUbicacion(
+      int $id
+  ): void
+  {
+    $stmt = $this->conectar()->prepare('DELETE FROM ubicacioncliente WHERE id = ?;');
+    $this->ejecutarSentencia($stmt, array($id));
   }
 }

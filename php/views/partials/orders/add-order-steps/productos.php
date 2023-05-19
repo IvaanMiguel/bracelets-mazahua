ee0737e7a66f646b810da83fdee6c8fc55ea8e61
Aivan <dominguezmiguel715@gmail.com>
2023-05-18 22:20:54 -0700

    HTML removido.

--- php/views/partials/orders/add-order-steps/productos.php
+++ php/views/partials/orders/add-order-steps/productos.php
@@ -5,29 +5,16 @@
       data-color-texto='var(--clr-secundario-10)'
       data-variante='texto-icono'
       data-icono='search'
-      data-etiqueta='Buscar producto'>
+      data-etiqueta='Buscar productos'>
   </boton-rellenado>
-  <info-detalles id='info-producto'></info-detalles>
-  <contenedor-flex gap='var(--espaciado-chico)'>
-    <campo-texto>
-      <wc-texto slot='etiqueta' data-tipo-fuente='etiqueta-l'>Cantidad</wc-texto>
-      <input type='text' name='cantidadProducto'>
-    </campo-texto>
-  </contenedor-flex>
-  <boton-delineado
-      data-color-texto='var(--clr-primario-40)'
-      data-etiqueta='Añadir producto'
-      data-icono='add'
-      data-variante='texto-icono'>
-  </boton-delineado>
   <wc-colapsable data-minicabecera>
     <wc-texto slot='texto-cabecera' data-tipo-fuente='titulo-s'>Productos añadidos</wc-texto>
-    <lista-controlador></lista-controlador>
+    <lista-controlador id='productos-agregados'></lista-controlador>
   </wc-colapsable>
-  <item-detalles id='total'>
+  <item-detalles>
     <wc-texto data-tipo-fuente='etiqueta-m'>Total</wc-texto>
     <wc-texto data-tipo-fuente='cuerpo-l'>
-      $<span id='cantidad'>0.00</span> MXN
+      $<span id='total'>0.00</span> MXN
     </wc-texto>
   </item-detalles>
 </contenedor-flex>

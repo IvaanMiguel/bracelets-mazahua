4fb3f53c508ae0e852da4f8f550bbbd300a00688
Aivan <dominguezmiguel715@gmail.com>
2023-05-18 22:21:58 -0700

    _

--- php/views/partials/orders/administrar-pedidos.php
+++ php/views/partials/orders/administrar-pedidos.php
@@ -1,6 +1,6 @@
 <lista-encabezada>
   <h1 class='titulo-grande' slot='titulo'>14 pedidos</h1>
-  <wc-colapsable slot='lista'>
+  <wc-colapsable>
     <wc-texto slot='texto-cabecera' data-tipo-fuente='titulo-m'>7 por entregar</wc-texto>
     <md-icono slot='icono-cabecera' data-icono='expand_less' data-opsz='24'></md-icono>
     <lista-controlador>
@@ -39,7 +39,7 @@
       </item-divisor>
     </lista-controlador>
   </wc-colapsable>
-  <wc-colapsable slot='lista'>
+  <wc-colapsable>
     <wc-texto slot='texto-cabecera' data-tipo-fuente='titulo-m'>7 entrgados</wc-texto>
     <md-icono slot='icono-cabecera' data-icono='expand_less' data-opsz='24'></md-icono>
     <lista-controlador>

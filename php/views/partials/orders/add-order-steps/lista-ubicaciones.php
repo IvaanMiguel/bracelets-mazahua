fa9382baacc33189bb95898b6834cd695f08bf63
Aivan <dominguezmiguel715@gmail.com>
2023-05-19 22:27:25 -0700

    Clase y elemento 'placeholder' añadido.

--- php/views/partials/orders/add-order-steps/lista-ubicaciones.php
+++ php/views/partials/orders/add-order-steps/lista-ubicaciones.php
@@ -1,6 +1,10 @@
 <lista-desplegable>
   <select class='ubicaciones-cliente'>
-    <option selected hidden value=''>Cargando ubicaciones...</option>
+    <option selected hidden value=''>Sin ubicaciones para mostrar.</option>
   </select>
 </lista-desplegable>
-<info-detalles></info-detalles>
+<info-detalles class='info-ubicacion'>
+  <contenedor-flex margin='auto' padding='var(--espaciado-chico)'>
+   <wc-texto data-tipo-fuente='etiqueta-l'>Ninguna ubicación seleccionada.</wc-texto>
+  </contenedor-flex>
+</info-detalles>

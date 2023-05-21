1de5d8bd4a4a5c9cdaeaaa116e94af17661f479a
Aivan <dominguezmiguel715@gmail.com>
2023-05-20 23:00:15 -0700

    Include agregado.

---
+++ php/includes/orders/agregar_pedido.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  var_dump($_POST);
+  // $idCliente = htmlspecialchars($_POST['idCliente'], ENT_QUOTES, 'UTF-8');
+  // $productos = $_POST['productos'];
+  // $callesAdyacentes = htmlspecialchars($_POST['callesAdyacentes'], ENT_QUOTES, 'UTF-8');
+  // $colonia = htmlspecialchars($_POST['colonia'], ENT_QUOTES, 'UTF-8');
+  // $numeroExterior = htmlspecialchars($_POST['numeroExterior'], ENT_QUOTES, 'UTF-8');
+  // $numeroInterior = htmlspecialchars($_POST['numeroInterior'], ENT_QUOTES, 'UTF-8');
+  // $cp = htmlspecialchars($_POST['cp'], ENT_QUOTES, 'UTF-8');
+  // $idCliente = htmlspecialchars($_POST['idCliente'], ENT_QUOTES, 'UTF-8');
+
+  // $ubicacion = Ubicacion::crearUbicacionConstructor(
+  //   $callePrincipal,
+  //   $callesAdyacentes,
+  //   $colonia,
+  //   $numeroExterior,
+  //   $numeroInterior,
+  //   $cp,
+  //   $idCliente
+  // );
+  // $ubicacion->registrarUbicacion();
+}

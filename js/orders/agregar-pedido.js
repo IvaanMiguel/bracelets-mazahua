de812a07665904715a66a90bd913543769c3147c
Aivan <dominguezmiguel715@gmail.com>
2023-05-21 23:09:58 -0700

    Promesas temporales añadidas.

--- js/orders/agregar-pedido.js
+++ js/orders/agregar-pedido.js
@@ -1,8 +1,15 @@
 import obtenerFormData from './obtener-form-data.js';
+import { crearNotificacion, obtenerRespuesta } from '../vista-control.js';
 
 document.addEventListener('hacerpedido', () => {
   fetch('php/includes/orders/agregar_pedido.inc.php', {
     method: 'POST',
     body: obtenerFormData()
-  });
+  })
+    .then((respuesta) => respuesta.json())
+    .then((datos) => {
+      obtenerRespuesta(datos, () => {
+        crearNotificacion(datos.contenido[0].titulo, datos.contenido[0].mensaje, 'error');
+      });
+    });
 });

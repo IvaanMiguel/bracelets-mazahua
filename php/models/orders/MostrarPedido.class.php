9a6c4b7ff923239eeb29d6a94f875cfa632feb24
Aivan <dominguezmiguel715@gmail.com>
2023-05-21 23:06:58 -0700

    Modelos para crear pedidos y mostrar pedidos pendients añadidos.

---
+++ php/models/orders/MostrarPedido.class.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace models\orders;
+
+use \classes\Dbh;
+use \classes\Respuesta;
+
+class MostrarPedido extends Dbh
+{
+  protected function seleccionarPedidosPendientes(): bool|string
+  {
+    $stmt = $this->conectar()->prepare('SELECT * FROM vwPedidosPendientes;');
+    $this->ejecutarSentencia($stmt);
+    $tuplas = $stmt->fetchAll();
+
+    if (count($tuplas) <= 0) {
+      return (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::MENSAJE, 'Sin pedidos pendientes'))->Json();
+    }
+
+    return (new Respuesta(Respuesta::STATUS_EXITO, Respuesta::ARRAY, $tuplas))->Json();
+  }
+}

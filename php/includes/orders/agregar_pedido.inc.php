45381581b627c1adbe3ea2b69111679f8864d7cb
Aivan <dominguezmiguel715@gmail.com>
2023-05-21 23:07:38 -0700

    Captura de campos y creación del pedido añadidos.

--- php/includes/orders/agregar_pedido.inc.php
+++ php/includes/orders/agregar_pedido.inc.php
@@ -1,24 +1,36 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  var_dump($_POST);
-  // $idCliente = htmlspecialchars($_POST['idCliente'], ENT_QUOTES, 'UTF-8');
-  // $productos = $_POST['productos'];
-  // $callesAdyacentes = htmlspecialchars($_POST['callesAdyacentes'], ENT_QUOTES, 'UTF-8');
-  // $colonia = htmlspecialchars($_POST['colonia'], ENT_QUOTES, 'UTF-8');
-  // $numeroExterior = htmlspecialchars($_POST['numeroExterior'], ENT_QUOTES, 'UTF-8');
-  // $numeroInterior = htmlspecialchars($_POST['numeroInterior'], ENT_QUOTES, 'UTF-8');
-  // $cp = htmlspecialchars($_POST['cp'], ENT_QUOTES, 'UTF-8');
-  // $idCliente = htmlspecialchars($_POST['idCliente'], ENT_QUOTES, 'UTF-8');
+use   controllers\orders\CrearPedido;
 
-  // $ubicacion = Ubicacion::crearUbicacionConstructor(
-  //   $callePrincipal,
-  //   $callesAdyacentes,
-  //   $colonia,
-  //   $numeroExterior,
-  //   $numeroInterior,
-  //   $cp,
-  //   $idCliente
-  // );
-  // $ubicacion->registrarUbicacion();
+require_once dirname(__DIR__) . '/../constantes.php';
+require_once AUTOLOADER;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $idCliente = htmlspecialchars($_POST['idCliente'], ENT_QUOTES, 'UTF-8');
+  $productos = json_decode($_POST['productos'], true);
+  $tipoEntrega = htmlspecialchars($_POST['tipoEntrega'], ENT_QUOTES, 'UTF-8');
+  $idUbicacion = htmlspecialchars($_POST['ubicacion'], ENT_QUOTES, 'UTF-8');
+  $nombreDestinatario = htmlspecialchars($_POST['nombreDestinatario'], ENT_QUOTES, 'UTF-8');
+  $celularDestinatario = htmlspecialchars($_POST['celularDestinatario'], ENT_QUOTES, 'UTF-8');
+  $fechaEntrega = htmlspecialchars($_POST['fechaEntrega'], ENT_QUOTES, 'UTF-8');
+  $horaEntrega = htmlspecialchars($_POST['horaEntrega'], ENT_QUOTES, 'UTF-8');
+  $aplicacion = htmlspecialchars($_POST['aplicacion'] ?? '', ENT_QUOTES, 'UTF-8');
+  $tipoPago = htmlspecialchars($_POST['tipoPago'] ?? '', ENT_QUOTES, 'UTF-8');
+  $detallesPago = 'Detalles placeholder';
+
+  $pedido = new CrearPedido(
+    $idCliente,
+    $tipoPago,
+    $detallesPago,
+    $productos,
+    $tipoEntrega,
+    $idUbicacion,
+    $aplicacion,
+    $nombreDestinatario,
+    $celularDestinatario,
+    $fechaEntrega,
+    $horaEntrega
+  );
+
+  $pedido->registrarPedido();
 }

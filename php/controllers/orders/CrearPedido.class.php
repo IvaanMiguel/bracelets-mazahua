50b51f6a7e3587abdd95c4e3ef8a3240bbe18af7
Aivan <dominguezmiguel715@gmail.com>
2023-05-22 21:58:03 -0700

    Autenticación para el registro de un pedido añadida.

--- php/controllers/orders/CrearPedido.class.php
+++ php/controllers/orders/CrearPedido.class.php
@@ -4,6 +4,7 @@ namespace controllers\orders;
 
 use \classes\Respuesta;
 use \controllers\orders\Mensaje;
+use \controllers\orders\PedidoAutenticacion;
 
 require_once dirname(__DIR__) . '/../constantes.php';
 require_once UTILS;
@@ -13,7 +14,6 @@ class CrearPedido extends \models\orders\CrearPedido
   private int $idCliente;
   private string $tipoPago;
   private ?string $detallesPago;
-  // private float $total;
   private array $productos;
   private string $tipoEntrega;
   private ?int $idUbicacion;
@@ -22,26 +22,30 @@ class CrearPedido extends \models\orders\CrearPedido
   private ?string $celularDestinatario;
   private string $fechaEntrega;
   private string $horaEntrega;
+  private string $clabeCuenta;
+  private string $numeroTarjeta;
+  private string $titular;
 
   public function __construct(
     int $idCliente,
     string $tipoPago,
     string $detallesPago,
-    // float $total,
     array $productos,
     string $tipoEntrega,
-    int $idUbicacion,
-    string $aplicacion,
+    ?int $idUbicacion,
+    ?string $aplicacion,
     string $nombreDestinatario,
     string $celularDestinatario,
     string $fechaEntrega,
-    string $horaEntrega
+    string $horaEntrega,
+    ?string $clabeCuenta,
+    ?string $numeroTarjeta,
+    ?string $titular
   )
   {
     $this->idCliente = $idCliente;
     $this->tipoPago = $tipoPago;
     $this->detallesPago = $detallesPago;
-    // $this->total = $total;
     $this->productos = $productos;
     $this->tipoEntrega = $tipoEntrega;
     $this->idUbicacion = $idUbicacion;
@@ -50,15 +54,46 @@ class CrearPedido extends \models\orders\CrearPedido
     $this->celularDestinatario = reemplazarEspacios($celularDestinatario);
     $this->fechaEntrega = $fechaEntrega;
     $this->horaEntrega = $horaEntrega;
+    $this->clabeCuenta = preg_replace('/\s+/', '', trim($clabeCuenta));
+    $this->numeroTarjeta = preg_replace('/\s+/', '', trim($numeroTarjeta));
+    $this->titular = reemplazarEspacios($titular);
   }
 
   public function registrarPedido(): void
   {
+    PedidoAutenticacion::validarCliente($this->idCliente);
+    PedidoAutenticacion::validarProductos($this->productos);
+    PedidoAutenticacion::validarNombreDestinatario($this->nombreDestinatario, $this->idCliente);
+    PedidoAutenticacion::validarCelularDestinatario($this->celularDestinatario, $this->idCliente);
+
+    switch ($this->tipoPago) {
+      case 'Efectivo':
+        $this->detallesPago = null;
+        break;
+
+      case 'Depósito':
+        PedidoAutenticacion::validarClabeCuenta($this->clabeCuenta);
+        break;
+
+      case 'Tarjeta':
+        PedidoAutenticacion::validarNombreTitular($this->titular);
+        PedidoAutenticacion::validarNumeroTarjeta($this->numeroTarjeta);
+        break;
+    }
+
+    if (count(PedidoAutenticacion::$errores) > 0) {
+      $respuesta = new Respuesta(
+        Respuesta::STATUS_ERROR,
+        Respuesta::ARRAY,
+        PedidoAutenticacion::$errores
+      );
+      exit($respuesta->Json());
+    }
+
     $idPedido = $this->crearPedido(
       $this->idCliente,
       $this->tipoPago,
       $this->detallesPago,
-      // $this->total,
       $this->productos,
       $this->tipoEntrega,
       $this->idUbicacion,

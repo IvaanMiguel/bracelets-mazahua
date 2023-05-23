b685afddf07016c389ea39406e3631d24a594c42
Aivan <dominguezmiguel715@gmail.com>
2023-05-22 22:11:05 -0700

    Condicionales para verificar existencias de productos y productos registrados.

--- php/views/partials/orders/popups/buscar-producto.php
+++ php/views/partials/orders/popups/buscar-producto.php
@@ -20,41 +20,48 @@
   </boton-icono>
   <contenedor-flex flex-direction='column' gap='var(--espaciado-grande)'>
     <contenedor-flex id='productos-contenido' gap='var(--espaciado-chico)'>
-      <?php foreach ($categoriasActivas as $categoriaActiva): ?>
-        <wc-colapsable data-minicabecera>
-          <wc-texto slot='texto-cabecera' data-tipo-fuente='titulo-s'>
-            <?= $categoriaActiva['nombreCategoria'] ?>
-          </wc-texto>
-          <lista-controlador id='lista-productos'>
-    
-            <?php
-              $productosFiltrados = array_filter($productos, function($producto) use ($categoriaActiva) {
-                return $producto['idCategoriaProducto'] === $categoriaActiva['idCategoriaProducto'];
-              });
+      <?php if (count($categoriasActivas) > 0): ?>
+        <?php foreach ($categoriasActivas as $categoriaActiva): ?>
+          <wc-colapsable data-minicabecera>
+            <wc-texto slot='texto-cabecera' data-tipo-fuente='titulo-s'>
+              <?= $categoriaActiva['nombreCategoria'] ?>
+            </wc-texto>
+            <lista-controlador id='lista-productos'>
       
-              $i = 0;
-            ?>
-    
-            <?php foreach ($productosFiltrados as $producto): ?>
-              <item-divisor <?= ++$i === count($productosFiltrados) ? 'data-no-divisor' : '' ?>>
-                <lista-item data-no-final>
-                  <item-detalles>
-                    <input class='check' slot='inicio' type='checkbox'>
-                    <wc-texto class='nombre'><?= $producto['nombreProducto'] ?></wc-texto>
-                    <wc-texto>$<span class='precio'><?=$producto['precio'] ?></span> MXN</wc-texto>
-                    <wc-texto>
-                      <span class='existencias'><?=$producto['existencias'] ?></span>
-                      existencia<?= $producto['existencias'] == 1 ? '' : 's' ?>
-                    </wc-texto>
-                  </item-detalles>
-                  <input class='id-producto' type='hidden' value=<?= $producto['idProducto'] ?>>
-                </lista-item>
-              </item-divisor>
-             <?php endforeach; ?>
-    
-          </lista-controlador>
-        </wc-colapsable>
-      <?php endforeach; ?>
+              <?php
+                $productosFiltrados = array_filter($productos, function($producto) use ($categoriaActiva) {
+                  return $producto['idCategoriaProducto'] === $categoriaActiva['idCategoriaProducto'];
+                });
+        
+                $i = 0;
+              ?>
+      
+              <?php foreach ($productosFiltrados as $producto): ?>
+                <?php if ($producto['existencias'] <= 0) continue; ?>
+                <item-divisor <?= ++$i === count($productosFiltrados) ? 'data-no-divisor' : '' ?>>
+                  <lista-item data-no-final>
+                    <item-detalles>
+                      <input class='check' slot='inicio' type='checkbox'>
+                      <wc-texto class='nombre'><?= $producto['nombreProducto'] ?></wc-texto>
+                      <wc-texto>$<span class='precio'><?=$producto['precio'] ?></span> MXN</wc-texto>
+                      <wc-texto>
+                        <span class='existencias'><?=$producto['existencias'] ?></span>
+                        existencia<?= $producto['existencias'] == 1 ? '' : 's' ?>
+                      </wc-texto>
+                    </item-detalles>
+                    <input class='id-producto' type='hidden' value=<?= $producto['idProducto'] ?>>
+                  </lista-item>
+                </item-divisor>
+              <?php endforeach; ?>
+      
+            </lista-controlador>
+          </wc-colapsable>
+        <?php endforeach; ?>
+      <?php else: ?>
+        <contenedor-flex margin='0 auto' padding='var(--espaciado-chico)'>
+          <wc-texto data-tipo-fuente='etiqueta-l'>Sin productos registrados.</wc-texto>
+        </contenedor-flex>
+      <?php endif; ?>
     </contenedor-flex>
     <boton-elevado
         data-evento='seleccionarproductos'

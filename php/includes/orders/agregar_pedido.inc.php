8f8cfefece239770148ce8c3b735fda3745c9a96
Aivan <dominguezmiguel715@gmail.com>
2023-05-22 22:06:01 -0700

    Adición de campos.

--- php/includes/orders/agregar_pedido.inc.php
+++ php/includes/orders/agregar_pedido.inc.php
@@ -1,22 +1,26 @@
 <?php
 
-use   controllers\orders\CrearPedido;
+use controllers\orders\CrearPedido;
 
 require_once dirname(__DIR__) . '/../constantes.php';
 require_once AUTOLOADER;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $idCliente = htmlspecialchars($_POST['idCliente'], ENT_QUOTES, 'UTF-8');
+  $idCliente = intval($_POST['idCliente']);
   $productos = json_decode($_POST['productos'], true);
   $tipoEntrega = htmlspecialchars($_POST['tipoEntrega'], ENT_QUOTES, 'UTF-8');
-  $idUbicacion = htmlspecialchars($_POST['ubicacion'], ENT_QUOTES, 'UTF-8');
+  $idUbicacion = $_POST['ubicacion'] ?? null;
   $nombreDestinatario = htmlspecialchars($_POST['nombreDestinatario'], ENT_QUOTES, 'UTF-8');
   $celularDestinatario = htmlspecialchars($_POST['celularDestinatario'], ENT_QUOTES, 'UTF-8');
   $fechaEntrega = htmlspecialchars($_POST['fechaEntrega'], ENT_QUOTES, 'UTF-8');
   $horaEntrega = htmlspecialchars($_POST['horaEntrega'], ENT_QUOTES, 'UTF-8');
-  $aplicacion = htmlspecialchars($_POST['aplicacion'] ?? '', ENT_QUOTES, 'UTF-8');
-  $tipoPago = htmlspecialchars($_POST['tipoPago'] ?? '', ENT_QUOTES, 'UTF-8');
-  $detallesPago = 'Detalles placeholder';
+  $aplicacion = $_POST['aplicacion'] ?? null;
+  $tipoPago = htmlspecialchars($_POST['tipoPago'], ENT_QUOTES, 'UTF-8');
+  $detallesPago = htmlspecialchars($_POST['detallesPago'] ?? null, ENT_QUOTES, 'UTF-8');
+  $detallesPago = htmlspecialchars($_POST['detallesPago'] ?? null, ENT_QUOTES, 'UTF-8');
+  $clabeCuenta = htmlspecialchars($_POST['clabeCuenta'] ?? null, ENT_QUOTES, 'UTF-8');
+  $numeroTarjeta = htmlspecialchars($_POST['numeroTarjeta'] ?? null, ENT_QUOTES, 'UTF-8');
+  $titular = htmlspecialchars($_POST['titular'] ?? null, ENT_QUOTES, 'UTF-8');
 
   $pedido = new CrearPedido(
     $idCliente,
@@ -29,7 +33,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nombreDestinatario,
     $celularDestinatario,
     $fechaEntrega,
-    $horaEntrega
+    $horaEntrega,
+    $clabeCuenta,
+    $numeroTarjeta,
+    $titular
   );
 
   $pedido->registrarPedido();
